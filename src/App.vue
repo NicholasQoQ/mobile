ebe34c31b5cdeacdd81dbd4fcb753c254c9ff437
NicholasQoQ <1123121227@qq.com>
2019-07-27 21:51:55 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    顶顶顶顶
+    <p>{{count}}</p>
+    <router-view />
+    <van-button type="primary">主要按钮</van-button>
+    <input type="button" value="点击" @click="add" />
+    <input type="button" value="点击22" @click="abc" />
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import { mapState, mapMutations, mapActions } from 'vuex'
+// import { mapState, mapMutations, mapActions } from 'vuex'
+export default {
+  computed: {
+    ...mapState(['count'])
+    // count () {
+    //   return this.$store.state.count
+    // }
+  },
+  methods: {
+    ...mapMutations(['add']),
+    // add () {
+    //   this.$store.commit('add')
+    // }
+    ...mapActions(['abc'])
+    // abc () {
+    //   this.$store.dispatch('abc')
+    // }
   }
+}
+</script>
+<style lang="less">
+#app{
+  width: 375px;
+  height: 200px;
+  background-color: red;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,37 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import 'amfe-flexible/index.min.js'
+Vue.use(Vant)
+
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
+
+const store = new Vuex.Store({
+  // ...
+  state: {
+    count: 100
+  },
+  mutations: {
+    add (state, newnum) {
+      console.log(newnum)
+      state.count = newnum
+    }
+  },
+  actions: {
+    abc (store) {
+      console.log(11)
+      setTimeout(() => {
+        const newnum = 500
+        store.commit('add', newnum)
+      }, 1000)
+    }
+  }
+})
 
 new Vue({
   router,
